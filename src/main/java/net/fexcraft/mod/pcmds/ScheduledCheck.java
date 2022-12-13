f1d1f24385a942a2d3b6fea6b5c25b0aef3b55fc
Ferdinand Calo <fercalo96@yahoo.de>
2022-12-13 22:00:41 +0100

    Update ScheduledCheck.java

--- src/main/java/net/fexcraft/mod/pcmds/ScheduledCheck.java
+++ src/main/java/net/fexcraft/mod/pcmds/ScheduledCheck.java
@@ -2,6 +2,8 @@ package net.fexcraft.mod.pcmds;
 
 import java.util.TimerTask;
 
+import net.fexcraft.lib.mc.utils.Static;
+
 /**
  * 
  * @author Ferdinand Calo' (FEX___96)
@@ -11,8 +13,21 @@ public class ScheduledCheck extends TimerTask {
 
 	@Override
 	public void run(){
-		// TODO Auto-generated method stub
-
+		Static.getServer().addScheduledTask(() -> {
+			PayableCommandSigns.FLOATING.values().removeIf(data -> {
+				try{
+					if(data.timer <= 0){
+						data.processTimed();
+						return true;
+					}
+					else data.timer--;
+				}
+				catch(Throwable e){
+					e.printStackTrace();
+				}
+				return false;
+			});
+		});
 	}
 
 }
