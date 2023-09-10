9a2a589f759acac2fb2cbc7702870569dbc70b6a
Ferdinand Calo <fercalo96@yahoo.de>
2023-09-10 20:15:03 +0200

    Update PCMDS_CON.java

--- src/main/java/net/fexcraft/mod/pcmds/PCMDS_CON.java
+++ src/main/java/net/fexcraft/mod/pcmds/PCMDS_CON.java
@@ -1,8 +1,11 @@
 package net.fexcraft.mod.pcmds;
 
+import net.fexcraft.lib.mc.capabilities.FCLCapabilities;
 import net.fexcraft.lib.mc.gui.GenericContainer;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.tileentity.TileEntitySign;
+import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
@@ -11,13 +14,17 @@ public class PCMDS_CON extends GenericContainer {
 	public final int x, y, z;
 	@SideOnly(Side.CLIENT)
 	public PCMDS_UI gui;
+	protected SignData data;
+	protected TileEntitySign sign;
 
 	public PCMDS_CON(EntityPlayer entity, int x, int y, int z){
 		super(entity);
 		this.x = x;
 		this.y = y;
 		this.z = z;
+		sign = (TileEntitySign)entity.world.getTileEntity(new BlockPos(x, y, z));
 		if(entity.world.isRemote) return;
+		data = sign.getCapability(FCLCapabilities.SIGN_CAPABILITY, null).getListener(SignCapImpl.class, SignCapImpl.REGNAME).data;
 	}
 
 	@Override
@@ -29,13 +36,16 @@ public class PCMDS_CON extends GenericContainer {
 			}
 		}
 		else{
-			//
+			if(packet.hasKey("signdata")){
+				data = new SignData(sign.signText.length).load(null, null, packet.getCompoundTag("signdata"));
+				gui.update();
+			}
 		}
 	}
 
 	private void sendSync(){
 		NBTTagCompound sync = new NBTTagCompound();
-		//
+		sync.setTag("signdata", data.save(new NBTTagCompound()));
 		send(Side.CLIENT, sync);
 	}
 
