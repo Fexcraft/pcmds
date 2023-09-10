5329e96a61eacbddfe4cda5d086d84fc3c652110
Ferdinand Calo <fercalo96@yahoo.de>
2023-09-10 20:52:41 +0200

    Update PCMDS_UI.java

--- src/main/java/net/fexcraft/mod/pcmds/PCMDS_UI.java
+++ src/main/java/net/fexcraft/mod/pcmds/PCMDS_UI.java
@@ -3,6 +3,8 @@ package net.fexcraft.mod.pcmds;
 import java.util.ArrayList;
 
 import net.fexcraft.lib.mc.gui.GenericGui;
+import net.fexcraft.mod.fsmm.util.Config;
+import net.fexcraft.mod.pcmds.SignData.Executor;
 import net.fexcraft.mod.pcmds.SignData.Type;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.nbt.NBTTagCompound;
@@ -30,7 +32,7 @@ public class PCMDS_UI extends GenericGui<PCMDS_CON> {
 			@Override
 			public boolean onclick(int mx, int my, int mb){
 				container.data.type = Type.BASIC;
-				update();
+				update(false);
 				return true;
 			}
 		});
@@ -39,26 +41,64 @@ public class PCMDS_UI extends GenericGui<PCMDS_CON> {
 			@Override
 			public boolean onclick(int mx, int my, int mb){
 				container.data.type = Type.RENT;
-				update();
+				update(false);
 				return true;
 			}
 		});
 		texts.put("type_rent", new BasicText(guiLeft + 84, guiTop + 40, 74, color, "Rent/Timed"));
+		//
 		buttons.put("time_days", new BasicButton("days", guiLeft + 162, guiTop + 38, 162, 38, 28, 12, true));
 		texts.put("time_days", new BasicText(guiLeft + 164, guiTop + 40, 76, color, "-d").autoscale().hoverable(true));
 		buttons.put("time_hours", new BasicButton("hours", guiLeft + 192, guiTop + 38, 192, 38, 28, 12, true));
 		texts.put("time_hours", new BasicText(guiLeft + 194, guiTop + 40, 76, color, "-h").autoscale().hoverable(true));
 		buttons.put("time_mins", new BasicButton("mins", guiLeft + 222, guiTop + 38, 222, 38, 28, 12, true));
 		texts.put("time_mins", new BasicText(guiLeft + 224, guiTop + 40, 76, color, "-m").autoscale().hoverable(true));
+		//
+		texts.put("executor", new BasicText(guiLeft + 8, guiTop + 58, 68, color, "Executor:"));
+		buttons.put("exe_server", new BasicButton("server", guiLeft + 80, guiTop + 56, 80, 56, 54, 12, true){
+			@Override
+			public boolean onclick(int mx, int my, int mb){
+				container.data.exec = Executor.SERVER;
+				update(false);
+				return true;
+			}
+		});
+		texts.put("exe_server", new BasicText(guiLeft + 82, guiTop + 58, 50, color, "Server"));
+		buttons.put("exe_player", new BasicButton("player", guiLeft + 138, guiTop + 56, 138, 56, 54, 12, true){
+			@Override
+			public boolean onclick(int mx, int my, int mb){
+				container.data.exec = Executor.PLAYER;
+				update(false);
+				return true;
+			}
+		});
+		texts.put("exe_player", new BasicText(guiLeft + 140, guiTop + 58, 50, color, "Player"));
+		buttons.put("exe_operator", new BasicButton("operator", guiLeft + 196, guiTop + 56, 196, 56, 54, 12, true){
+			@Override
+			public boolean onclick(int mx, int my, int mb){
+				container.data.exec = Executor.OPERATOR;
+				update(false);
+				return true;
+			}
+		});
+		texts.put("exe_operator", new BasicText(guiLeft + 198, guiTop + 58, 50, color, "Operator"));
+		texts.put("operator", new BasicText(guiLeft + 8, guiTop + 72, 68, color, "Name/UUID"));
+		fields.put("operator", new TextField(1, fontRenderer, guiLeft + 80, guiTop + 70, 170, 12));
+		//
 		container.gui = this;
 		NBTTagCompound com = new NBTTagCompound();
 		com.setBoolean("sync", true);
 		container.send(Side.SERVER, com);
 	}
 
-	public void update(){
+	public void update(boolean packet){
+		BasicButton button;
+		if(packet){
+			fields.get("fee").setText(Config.getWorthAsString(container.data.price, false));
+			fields.get("operator").setText(container.opname);
+		}
 		if(container.data.type == Type.BASIC){
-			BasicButton button = buttons.get("type_basic");
+			button = buttons.get("type_basic");
 			button.tx = 2;
 			button.ty = 226;
 			button = buttons.get("type_rent");
@@ -69,7 +109,7 @@ public class PCMDS_UI extends GenericGui<PCMDS_CON> {
 			texts.get("time_mins").string = "-";
 		}
 		else{
-			BasicButton button = buttons.get("type_basic");
+			button = buttons.get("type_basic");
 			button.tx = 80;
 			button.ty = 24;
 			button = buttons.get("type_rent");
@@ -81,6 +121,42 @@ public class PCMDS_UI extends GenericGui<PCMDS_CON> {
 			texts.get("time_days").string = "0d";
 			texts.get("time_hours").string = "0h";
 			texts.get("time_mins").string = "0m";
+		}
+		if(container.data.exec == Executor.OPERATOR){
+			button = buttons.get("exe_operator");
+			button.tx = 200;
+			button.ty = 226;
+			button = buttons.get("exe_player");
+			button.tx = 138;
+			button.ty = 56;
+			button = buttons.get("exe_server");
+			button.tx = 80;
+			button.ty = 56;
+			fields.get("operator").setVisible(true);
+			texts.get("operator").visible = true;
+		}
+		else{
+			button = buttons.get("exe_operator");
+			button.tx = 196;
+			button.ty = 56;
+			if(container.data.exec == Executor.PLAYER){
+				button = buttons.get("exe_player");
+				button.tx = 142;
+				button.ty = 226;
+				button = buttons.get("exe_server");
+				button.tx = 80;
+				button.ty = 56;
+			}
+			else{
+				button = buttons.get("exe_player");
+				button.tx = 138;
+				button.ty = 56;
+				button = buttons.get("exe_server");
+				button.tx = 84;
+				button.ty = 226;
+			}
+			fields.get("operator").setVisible(false);
+			texts.get("operator").visible = false;
 		}
 	}
 
