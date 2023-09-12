5a1d1cf463aacabd07b2a035d2e6ada134ff3670
Ferdinand Calo <fercalo96@yahoo.de>
2023-09-12 17:47:11 +0200

    Update PCMDS_CON.java

--- src/main/java/net/fexcraft/mod/pcmds/PCMDS_CON.java
+++ src/main/java/net/fexcraft/mod/pcmds/PCMDS_CON.java
@@ -1,7 +1,11 @@
 package net.fexcraft.mod.pcmds;
 
+import java.util.UUID;
+
+import com.mojang.authlib.GameProfile;
 import net.fexcraft.lib.mc.capabilities.FCLCapabilities;
 import net.fexcraft.lib.mc.gui.GenericContainer;
+import net.fexcraft.lib.mc.utils.Print;
 import net.fexcraft.lib.mc.utils.Static;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.nbt.NBTTagCompound;
@@ -9,6 +13,7 @@ import net.minecraft.tileentity.TileEntitySign;
 import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.server.permission.PermissionAPI;
 
 public class PCMDS_CON extends GenericContainer {
 
@@ -27,6 +32,10 @@ public class PCMDS_CON extends GenericContainer {
 		sign = (TileEntitySign)entity.world.getTileEntity(new BlockPos(x, y, z));
 		if(entity.world.isRemote) return;
 		data = sign.getCapability(FCLCapabilities.SIGN_CAPABILITY, null).getListener(SignCapImpl.class, SignCapImpl.REGNAME).data;
+		if(!PermissionAPI.hasPermission(player, PayableCommandSigns.EDIT_SIGN_PERM)){
+			Print.chat(player, EditCmd.trs("no_permission"));
+			player.closeScreen();
+		}
 	}
 
 	@Override
@@ -35,6 +44,46 @@ public class PCMDS_CON extends GenericContainer {
 			if(packet.getBoolean("sync")){
 				sendSync();
 				return;
+			}
+			if(packet.hasKey("update")){
+				if(!PermissionAPI.hasPermission(player, PayableCommandSigns.EDIT_SIGN_PERM)){
+					Print.chat(player, EditCmd.trs("no_permission"));
+					return;
+				}
+				SignCapImpl impl = sign.getCapability(FCLCapabilities.SIGN_CAPABILITY, null).getListener(SignCapImpl.class, SignCapImpl.REGNAME);
+				data.load(impl, sign, packet.getCompoundTag("update"));
+				if(packet.hasKey("exec")){
+					UUID uuid = null;
+					try{
+						uuid = UUID.fromString(packet.getString("exec"));
+					}
+					catch(Exception e){
+						e.printStackTrace();
+					}
+					if(uuid == null){
+						GameProfile gp = Static.getServer().getPlayerProfileCache().getGameProfileForUsername(packet.getString("exec"));
+						if(gp != null) uuid = gp.getId();
+					}
+					if(uuid == null){
+						Print.chat(player, EditCmd.trs("exec_not_found"));
+					}
+					else data.exid = uuid;
+				}
+				if(packet.getBoolean("activate")){
+					if(impl == null){
+						Print.chat(player, EditCmd.trs("sign_none_selected"));
+					}
+					else{
+						if(!impl.data.valid()){
+							Print.chat(player, EditCmd.trs("sign_incomplete"));
+						}
+						else{
+							impl.setActive(sign);
+							Print.chat(player, EditCmd.trs("sign_activated"));
+						}
+					}
+				}
+				player.closeScreen();
 			}
 		}
 		else{
