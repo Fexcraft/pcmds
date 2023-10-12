1cd9882ba310bd74a9e3008784e6f82e330537c7
Ferdinand Calo <fercalo96@yahoo.de>
2023-10-12 23:45:34 +0200

    Update SignData.java

--- src/main/java/net/fexcraft/mod/pcmds/SignData.java
+++ src/main/java/net/fexcraft/mod/pcmds/SignData.java
@@ -12,7 +12,7 @@ import net.fexcraft.lib.common.math.Time;
 import net.fexcraft.lib.mc.capabilities.FCLCapabilities;
 import net.fexcraft.lib.mc.utils.Print;
 import net.fexcraft.lib.mc.utils.Static;
-import net.fexcraft.mod.fsmm.api.Account;
+import net.fexcraft.mod.fsmm.data.Account;
 import net.fexcraft.mod.fsmm.util.DataManager;
 import net.fexcraft.mod.pcmds.PayableCommandSigns.DimPos;
 import net.minecraft.block.state.IBlockState;
